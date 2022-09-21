84fb0cd81db2f21570679d15a85e8f4ef674c27b
Ethan <etgeorge@davidson.edu>
2022-09-21 09:03:50 -0400

    In class demo

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,30 @@
 /* javascript */
 // $("h1").hide();
 
-let username=""
+let username = ""
+let requiredUsername = "secret"
 
-$(".submit").click(function(e){
+$(".submit").click(function (e) {
     e.preventDefault();
     username = $(".username").val();
-    console.log(username+" is username");
+    console.log(username + " is username");
+
+    let str = ""
+    if (username == requiredUsername) {
+        console.log("Success!");
+        str = `
+        <div class="alert alert-success" role="alert">
+            Welcome, compatriot ${username}
+        </div>
+        `
+    } else{
+        str=`
+        <div class="alert alert-danger" role="alert">
+            Invalid username. Try again?
+        </div>
+        `
+    }
+
+    $(".output").html(str);
+
 });
