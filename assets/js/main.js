eb8bc965a7151d2654f5c746bfded01bc9858e75
Ethan <etgeorge@davidson.edu>
2022-09-25 21:25:56 -0400

    Janky form validation, but it works

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,27 +2,38 @@
 /* javascript */
 // $("h1").hide();
 
-$("#answer").hide();
+$(".answer").hide();
 
 let yourMilk = "";
 
-$("form").submit(function(e) {
-  e.preventDefault();
-  yourMilk = ("#yourMilk").val;
+$("form").submit(function (e) {
+    e.preventDefault();
 
-  console.log(yourMilk);
 });
 
-$(".check").click(function (){
+
+$("#first").submit(function () {
+    console.log("Submitted");
+})
+$(".check").click(function () {
     $("#first").submit();
     let you = $("#yourMilk").val();
     let friend = $("#friend").val();
-    let value = you*friend;
-    
-    $("#answer").text(`${you} x ${friend} = ${value} cartons`);
-    $("#answer").show();
+    let value = you * friend;
+
+    $("#response").text(`${you} x ${friend} = ${value} carton(s)`);
+
+    let notValid = you.length === 0 || friend.length === 0 || you<0 || you>1 || friend<0 || friend >10
+
+    if(you<0 || you>1 || you.length===0){
+        alert("Please make first field less than 1 and greater than 0")
+    }
+    if (!notValid) {
+        $(".answer").show();
+    } else{
+        $(".answer").hide();
+
+    }
 })
 
-$("#first").submit(function (){
-    console.log("form submitted");
-})
+
